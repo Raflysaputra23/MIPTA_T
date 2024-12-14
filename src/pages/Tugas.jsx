7d05e1767bf7dc232094afdd9c531b1ce7c8e4a4
M. Rafly Saputra <966raflisaputra@gmail.com>
2024-12-14 19:13:47 +0700

    memperbarui UI

--- src/pages/Tugas.jsx
+++ src/pages/Tugas.jsx
@@ -200,6 +200,7 @@ const Tugas = () => {
             style: {
               maxHeight: 48 * 4.5,
               width: '4rem',
+              boxShadow: '-1px 1px 4px -1px rgba(0,0,0,0.2)',
             },
           },
         }}
