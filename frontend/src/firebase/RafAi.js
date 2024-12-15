95493bc73ae8991ceed863090cf329827bb70a63
M. Rafly Saputra <966raflisaputra@gmail.com>
2024-12-15 12:06:55 +0700

    memperbaru endpoint

--- frontend/src/firebase/RafAi.js
+++ frontend/src/firebase/RafAi.js
@@ -8,7 +8,7 @@ const { prompt } = {
 const RafAi = async (message, session) => {
     return new Promise( async (resolve, reject) => {
         try {
-            const { data } = await axios(`http://localhost:8000/API/RafAi`, {
+            const { data } = await axios(`https://mipta-t.vercel.app/API/RafAi`, {
                 method: "POST",
                 data: {
                     message,
